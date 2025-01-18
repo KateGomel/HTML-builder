5d741248b9b5d25bc65df7d326095af20e43a8aa
KateGomel <ykatuay@gmail.com>
2025-01-18 23:25:53 +0300

    feat: add solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const folderName = path.join(__dirname, 'secret-folder');
+
+async function listFiles(folder) {
+  try {
+    const filesArr = await fs.readdir(folder, {
+      withFileTypes: true,
+    });
+
+    for (const file of filesArr) {
+      if (file.isFile()) {
+        const filePath = path.join(folder, file.name);
+        const stats = await fs.stat(filePath);
+        const size = stats.size / 1024;
+        const ext = path.extname(file.name);
+        console.log(`${file.name} - ${ext} - ${size.toFixed(3)} kb`);
+      }
+    }
+  } catch (error) {
+    console.log('error:', error.message);
+  }
+}
+
+listFiles(folderName);
